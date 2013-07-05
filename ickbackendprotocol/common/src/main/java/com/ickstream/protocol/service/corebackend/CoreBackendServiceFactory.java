95940c966b3e9b05c5cd805c0cecd8325ee7c400
Erland Isaksson <erland@isaksson.info>
2013-07-05 11:21:50 +0200

    Performance optimization by caching Core Backend service client instance

--- ickbackendprotocol/common/src/main/java/com/ickstream/protocol/service/corebackend/CoreBackendServiceFactory.java
+++ ickbackendprotocol/common/src/main/java/com/ickstream/protocol/service/corebackend/CoreBackendServiceFactory.java
@@ -2,8 +2,13 @@ package com.ickstream.protocol.service.corebackend;
 
 import com.ickstream.protocol.common.ServiceFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CoreBackendServiceFactory extends ServiceFactory {
     private static final String COREBACKENDSERVICE_ENDPOINT = "http://api.ickstream.com/ickstream-cloud-core/backend/jsonrpc";
+
+    private static final Map<String, CoreBackendServiceImpl> CORE_BACKEND_SERVICE_CACHE = new HashMap<String, CoreBackendServiceImpl>();
 
     private static String getCoreBackendServiceEndpoint() {
         String url = System.getProperty("ickstream-core-backend-url");
@@ -14,9 +19,13 @@ public class CoreBackendServiceFactory extends ServiceFactory {
         }
     }
 
-    public static CoreBackendService getCoreBackendService(String accessToken) {
-        CoreBackendServiceImpl coreService = new CoreBackendServiceImpl(createHttpClient(), getCoreBackendServiceEndpoint());
-        coreService.setAccessToken(accessToken);
+    public static synchronized CoreBackendService getCoreBackendService(String accessToken) {
+        CoreBackendServiceImpl coreService = CORE_BACKEND_SERVICE_CACHE.get(accessToken);
+        if (coreService == null) {
+            coreService = new CoreBackendServiceImpl(createHttpClient(), getCoreBackendServiceEndpoint());
+            coreService.setAccessToken(accessToken);
+            CORE_BACKEND_SERVICE_CACHE.put(accessToken, coreService);
+        }
         return coreService;
     }
 }
