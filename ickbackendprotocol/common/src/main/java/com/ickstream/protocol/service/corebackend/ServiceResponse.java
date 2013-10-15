82e96970c00bd8eed7956171d5b3a8357fe8da06
Erland Isaksson <erland@isaksson.info>
2013-10-15 07:00:19 +0200

    Added possibility to return service attributes for services

--- ickbackendprotocol/common/src/main/java/com/ickstream/protocol/service/corebackend/ServiceResponse.java
+++ ickbackendprotocol/common/src/main/java/com/ickstream/protocol/service/corebackend/ServiceResponse.java
@@ -6,6 +6,8 @@
 package com.ickstream.protocol.service.corebackend;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ServiceResponse implements Serializable {
     private String id;
@@ -19,6 +21,7 @@ public class ServiceResponse implements Serializable {
     private String customData2;
     private String customData3;
     private String customData4;
+    private Map<String, Map<String, String>> attributes = new HashMap<String, Map<String, String>>();
 
     public String getId() {
         return id;
@@ -106,5 +109,13 @@ public class ServiceResponse implements Serializable {
 
     public void setCustomData4(String customData4) {
         this.customData4 = customData4;
+    }
+
+    public Map<String, Map<String, String>> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Map<String, Map<String, String>> attributes) {
+        this.attributes = attributes;
     }
 }
