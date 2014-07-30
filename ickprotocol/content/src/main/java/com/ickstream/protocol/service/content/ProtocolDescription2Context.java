99c6c8431afdf3f29a06784ca866d7d436412f36
Erland Isaksson <erland@isaksson.info>
2014-07-30 18:24:25 +0200

    Removed images attribute from getProtocolDescription2 response

--- ickprotocol/content/src/main/java/com/ickstream/protocol/service/content/ProtocolDescription2Context.java
+++ ickprotocol/content/src/main/java/com/ickstream/protocol/service/content/ProtocolDescription2Context.java
@@ -5,17 +5,12 @@
 
 package com.ickstream.protocol.service.content;
 
-import com.ickstream.protocol.service.ImageReference;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class ProtocolDescription2Context {
     private String contextId;
     private String name;
-    private List<ImageReference> images = new ArrayList<ImageReference>();
     private Map<String, Map<String, RequestDescription2>> supportedRequests = new HashMap<String, Map<String, RequestDescription2>>();
 
     public ProtocolDescription2Context() {
@@ -24,12 +19,6 @@ public class ProtocolDescription2Context {
     public ProtocolDescription2Context(String contextId, String name) {
         this.contextId = contextId;
         this.name = name;
-    }
-
-    public ProtocolDescription2Context(String contextId, String name, List<ImageReference> images) {
-        this.contextId = contextId;
-        this.name = name;
-        this.images = images;
     }
 
     public String getContextId() {
@@ -54,13 +43,5 @@ public class ProtocolDescription2Context {
 
     public void setSupportedRequests(Map<String, Map<String, RequestDescription2>> supportedRequests) {
         this.supportedRequests = supportedRequests;
-    }
-
-    public List<ImageReference> getImages() {
-        return images;
-    }
-
-    public void setImages(List<ImageReference> images) {
-        this.images = images;
     }
 }
