83ff15f41339fd6efb55f5f98f9a441ebde794ff
Erland Isaksson <erland@isaksson.info>
2014-08-02 14:38:47 +0200

    Corrected to mainCategory attribute is set in getServiceInformation response

--- ickbackendprotocol/common/src/main/java/com/ickstream/protocol/backend/common/AbstractCloudService.java
+++ ickbackendprotocol/common/src/main/java/com/ickstream/protocol/backend/common/AbstractCloudService.java
@@ -64,7 +64,7 @@ public abstract class AbstractCloudService implements CloudService {
             serviceInformation.setId(service.getId());
             serviceInformation.setName(service.getName());
             serviceInformation.setType(service.getType());
-            serviceInformation.getMainCategory();
+            serviceInformation.setMainCategory(service.getMainCategory());
             serviceInformation.setUrl(service.getUrl());
             serviceInformation.setImages(getImages());
             return serviceInformation;
