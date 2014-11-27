3ccd91bf0518de1b228ab1cbd139a5a37d4aa65f
Erland Isaksson <erland@isaksson.info>
2014-11-27 05:57:52 +0100

    Added so userId is saved after addDevice Added so userId and cloudCoreStatus is returned from getPlayerConfiguration Added so userId is returned from getPlayerStatus

--- ickplayer/src/main/java/com/ickstream/player/service/PlayerCommandService.java
+++ ickplayer/src/main/java/com/ickstream/player/service/PlayerCommandService.java
@@ -132,6 +132,7 @@ public class PlayerCommandService {
                 @Override
                 public void onMessage(AddDeviceResponse response) {
                     player.setAccessToken(response.getAccessToken());
+                    player.setUserId(response.getUserId());
                 }
 
                 @Override
@@ -170,6 +171,12 @@ public class PlayerCommandService {
         response.setHardwareId(player.getHardwareId());
         response.setPlayerModel(player.getModel());
         response.setCloudCoreUrl(player.getCloudCoreUrl());
+        if (player != null && player.hasAccessToken()) {
+            response.setCloudCoreStatus(CloudCoreStatus.REGISTERED);
+            response.setUserId(player.getUserId());
+        }else {
+            response.setCloudCoreStatus(CloudCoreStatus.UNREGISTERED);
+        }
         return response;
     }
 
@@ -194,6 +201,7 @@ public class PlayerCommandService {
         response.setPlaybackQueueMode(playerStatus.getPlaybackQueueMode());
         if (player != null && player.hasAccessToken()) {
             response.setCloudCoreStatus(CloudCoreStatus.REGISTERED);
+            response.setUserId(player.getUserId());
         } else {
             response.setCloudCoreStatus(CloudCoreStatus.UNREGISTERED);
         }
